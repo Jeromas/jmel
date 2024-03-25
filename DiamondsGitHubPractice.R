5b5ab3653b7dc4440adb7a6e4cebe8b4d22e3330
Jeromas <117402387+Jeromas@users.noreply.github.com>
2024-03-25 18:09:46 -0400

    Update DiamondsGitHubPractice.R

--- DiamondsGitHubPractice.R
+++ DiamondsGitHubPractice.R
@@ -2,5 +2,5 @@ library(tidyverse)
 library(dplyr)
 diamonds
 df <- read.csv('diamonds.csv')
-# Display the first 30 rows of variables x, y, and z
+# Display the first 40 rows of variables x, y, and z
 print(df[1:30, c('x', 'y', 'z')])
